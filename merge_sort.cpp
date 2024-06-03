4eb25a4d7bd13c65ef27dcb50d02884ab71e0f62
aritra-chy <123829017+aritra-chy@users.noreply.github.com>
2024-06-03 22:11:26 +0600

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,47 +1,104 @@
+/*------------------------------------------------------
+    author    : Aritra Chowdhury
+    created   : Monday | 03 June,2024 | 21:47:23 
+------------------------------------------------------*/
+ 
 #include <bits/stdc++.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
 using namespace std;
+#define read(type) readInt<type>() // Fast read
+#define ll long long
+#define nL "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 
-void merge(int arr[], int p, int q, int r)
+void merge(vector<int>&a, int low, int mid, int high)
 {
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    int L[n1], R[n2];
-    for (int i = 0; i < n1; i++)    
-        L[i] = arr[p + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[q + j + 1];
-    int i = 0, j = 0, k = p;
-    while (i < n1 && j < n2)
+    vector<int>temp;
+    int left = low;
+    int right = mid+1;
+
+    while(left <= mid && right <= high)
     {
-        if (L[i] <= R[j])
-            arr[k++] = L[i++];
+        if(a[left]<=a[right])
+        {
+            temp.push_back(a[left]);
+            left++;
+        }
         else
-            arr[k++] = R[j++];
+        {
+            temp.push_back(a[right]);
+            right++;
+        }
     }
-    while (i < n1)
-        arr[k++] = L[i++];
-    while (j < n2)
-        arr[k++] = R[j++];
-}
-void mergeSort(int arr[], int p, int r)
-{
-    if (p < r)
+
+    while(left <= mid)
     {
-        int q = (p + r) / 2;
-        mergeSort(arr, p, q);
-        mergeSort(arr, q + 1, r);
-        merge(arr, p, q, r);
+        temp.push_back(a[left]);
+        left++;
     }
+
+    while(right <= high)
+    {
+        temp.push_back(a[right]);
+        right++;
+    }
+
+    for(int i=low; i<=high; i++)
+    {
+        a[i] = temp[i-low];
+    }
+}
+
+void mergeSort(vector<int>&a, int low, int high)
+{
+    if(low >= high) return;
+
+    int mid = (low+high)/2;
+    mergeSort(a,low, mid);
+    mergeSort(a,mid+1,high);
+    merge(a,low,mid,high);
 }
 
 int main()
 {
     int n;
-    cin >> n;
-    int arr[n]; 
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    mergeSort(arr, 0, n - 1);
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    cin>>n;
+
+    vector<int>a(n);
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    mergeSort(a,0,n-1);
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    cout<<endl;
 }
